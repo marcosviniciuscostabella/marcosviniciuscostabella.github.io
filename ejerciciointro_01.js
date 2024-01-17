e01f38e91dc331606caf0bb62995e92d228ddb67
marcosviniciuscostabella <150136444+marcosviniciuscostabella@users.noreply.github.com>
2024-01-17 18:04:45 +0100

    subida2

---
+++ ejerciciointro_01.js
@@ -0,0 +1,12 @@
+let alumno = {
+    nombre : "Mercedes",
+    edad: 25
+}
+
+let mascota1 = {
+    nombre : "Naranjito",
+    forma : "Esférica",
+    especie : "Naranja"
+}
+console.log(mascota1);
+mascota1.peso = 15;
